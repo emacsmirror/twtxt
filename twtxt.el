cda79a622587649f3ae3b1394e286e5b27ba0f4a
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2020-08-23 11:27:50 +0700

    checkdoc

--- twtxt.el
+++ twtxt.el
@@ -17,86 +17,86 @@
 (require 'cl-lib)
 (require 'request)
 
-(defgroup twtxt nil 
-  "Client twtxt" 
+(defgroup twtxt nil
+  "Client twtxt"
   :group 'twtxt)
 
-(defvar twtxt-file "~/twtxt" 
-  "Path to twtxt file")
+(defvar twtxt-file "~/twtxt"
+  "Pafth to twtxt file.")
 
-(defvar twtxt-following nil 
-  "Following list")
+(defvar twtxt-following nil
+  "Following list.")
 
-(defvar twtxt-timeline-list nil 
-  "Timeline list")
+(defvar twtxt-timeline-list nil
+  "Timeline list.")
 
-(defvar twtxt-username "" 
-  "Temporary storage of username")
+(defvar twtxt-username ""
+  "Temporary storage of username.")
 
-(defun twtxt-get-datetime () 
-  "Getting date and time according to RFC 3339 standard"
-  (concat (format-time-string "%Y-%m-%dT%T") 
-	  ((lambda (x) 
-	     (concat (substring x 0 3) ":" (substring x 3 5))) 
+(defun twtxt-get-datetime ()
+  "Getting date and time according to RFC 3339 standard."
+  (concat (format-time-string "%Y-%m-%dT%T")
+	  ((lambda (x)
+	     (concat (substring x 0 3) ":" (substring x 3 5)))
 	   (format-time-string "%z"))))
 
-(defun twtxt-parse-text (text) 
-  "Convert text to list post"
+(defun twtxt-parse-text (text)
+  "Convert TEXT to list post."
   (split-string text "\n"))
 
-(defun twtxt-replace-tab (str) 
-  "Replacing tabs with line breaks"
+(defun twtxt-replace-tab (str)
+  "Replacing tabs with line breaks in STR."
   (replace-regexp-in-string "\t" "\n" str))
 
-(defun twtxt-sort-post (list) 
-  "Sorting posts"
+(defun twtxt-sort-post (list)
+  "Sorting posts in LIST."
   (sort list #'string>))
 
-(defun twtxt-append-username (text) 
-  "Append username in posts"
-  (mapcar (lambda (item) 
-	    (concat twtxt-username "\t" item)) 
+(defun twtxt-append-username (text)
+  "Append username in TEXT."
+  (mapcar (lambda (item)
+	    (concat twtxt-username "\t" item))
 	  (twtxt-parse-text text)))
 
-(defun twtxt-push-text (text) 
-  "Concatenate the resulting list with the current list" 
+(defun twtxt-push-text (text)
+  "Concatenate the resulting TEXT with the current list."
   (setq twtxt-timeline-list (append twtxt-timeline-list (twtxt-append-username text))))
 
-(defun twtxt-fetch (url) 
-  "Get text"
-  (progn (request url 
-	   :parser 'buffer-string 
-	   :success (cl-function (lambda 
-				   (&key 
+(defun twtxt-fetch (url)
+  "Getting text by URL."
+  (progn (request url
+	   :parser 'buffer-string
+	   :success (cl-function (lambda
+				   (&key
 				    data
-				    &allow-other-keys) 
+				    &allow-other-keys)
 				   (twtxt-push-text data))))))
 
-(defun twtxt-fetch-list () 
-  "Getting a list of texts"
-  (mapcar (lambda (item) 
-	    (setq twtxt-username (concat "[[" (car (cdr item)) "][" (car item) "]]")) 
+(defun twtxt-fetch-list ()
+  "Getting a list of texts."
+  (mapcar (lambda (item)
+	    (setq twtxt-username (concat "[[" (car (cdr item)) "][" (car item) "]]"))
 	    (twtxt-fetch (car (cdr item)))) twtxt-following))
 
-(defun twtxt-timeline-buffer (data) 
-  "Create buffer"
-  (switch-to-buffer (get-buffer-create "*twtxt-timeline*")) 
-  (mapcar (lambda (item) 
-	    (insert (twtxt-replace-tab item)) 
-	    (insert "\n\n")) data) 
-  (org-mode) 
+(defun twtxt-timeline-buffer (data)
+  "Create buffer and DATA recording."
+  (switch-to-buffer (get-buffer-create "*twtxt-timeline*"))
+  (mapcar (lambda (item)
+	    (insert (twtxt-replace-tab item))
+	    (insert "\n\n")) data)
+  (org-mode)
   (beginning-of-buffer))
 
-(defun twtxt-timeline () 
-  "View your timeline" 
-  (interactive) 
-  (twtxt-fetch-list) 
-  (twtxt-timeline-buffer (twtxt-sort-post twtxt-timeline-list)) 
+(defun twtxt-timeline ()
+  "View your timeline."
+  (interactive)
+  (twtxt-fetch-list)
+  (twtxt-timeline-buffer (twtxt-sort-post twtxt-timeline-list))
   (setq twtxt-timeline-list nil))
 
-(defun twtxt-post (post) 
-  "Post a status update" 
-  (interactive "sPost:") 
+(defun twtxt-post (post)
+  "POST a status update."
+  (interactive "sPost:")
   (append-to-file (concat (twtxt-get-datetime) "\t" post "\n") nil twtxt-file))
 
 (provide 'twtxt)
