8cadba69057e6f1fcbfb106736c5e7ee1068ff46
Andros Fenollosa <andros@fenollosa.email>
2025-01-29 11:46:57 +0100

    Updated paginator

--- twtxt-timeline.el
+++ twtxt-timeline.el
@@ -51,7 +51,8 @@
   (require 'wid-edit))
 
 ;; Variables
-(defvar twtxt--twtxts-per-page 10)
+(defvar twtxt--widgets '())
+(defvar twtxt--twtxts-per-page 5)
 (defvar twtxt--twtxts-page 1)
 (defconst twtxt--timeline-name-buffer "*twtxt - Timeline*")
 (defvar twtxt--timeline-separator
@@ -59,36 +60,38 @@
    (window-width) ?\u2500))
 
 ;; Functions
+(defun twtxt-recalculate-timeline-separator ()
+  "Recalculate the timeline separator."
+  (setq twtxt--timeline-separator
+	(make-string
+	 (window-width) ?\u2500)))
 
-;; Layout
-(defun twtxt--timeline-layout ()
-  "Create the main layout for the welcome screen."
-  (switch-to-buffer twtxt--timeline-name-buffer)
-  (kill-all-local-variables)
-  (let ((inhibit-read-only t))
-    (erase-buffer))
-  (remove-overlays)
-  (erase-buffer)
-  ;; Controls
-  (widget-insert "\n")
-  (widget-create 'push-button
-		 :notify (lambda (&rest ignore)
-			   (twtxt--post-buffer))
-		 :help-echo "Publish a new twtxt post."
-		 " ＋ New post ")
-  (widget-insert " ")
-  (widget-create 'push-button
-		 :notify (lambda (&rest ignore)
-			   (twtxt-timeline))
-		 " ↺ Refresh timeline ")
-  (widget-insert " ")
-  (widget-create 'push-button
-		 :notify (lambda (&rest ignore)
-			   (twtxt-timeline))
-		 " 🖼 Show profile ")
-  (widget-insert "\n\n")
-  (widget-insert twtxt--timeline-separator)
-  (widget-insert "\n\n")
+(defun twtxt--clear-txts ()
+  "Clear the list of twtxts."
+  (let ((line-from-twtxt 5))
+    (when (>= (line-number-at-pos (point-max)) line-from-twtxt)
+      (let ((inhibit-read-only t))
+	;; Delete inserts: From line line-from-twtxt to the end
+	(goto-line line-from-twtxt)
+	(delete-region (point) (point-max))
+	;; Delete old widgets
+	(dolist (widget-item twtxt--widgets)
+	  (widget-delete widget-item))
+	(setq twtxt--widgets '())))))
+
+(defun twtxt--previous-page ()
+  "Go to the previous page of twtxts."
+  (setq twtxt--twtxts-page (1- twtxt--twtxts-page))
+  (twtxt--redraw-timeline))
+
+(defun twtxt--next-page ()
+  "Go to the next page of twtxts."
+  (setq twtxt--twtxts-page (1+ twtxt--twtxts-page))
+  (twtxt--redraw-timeline))
+
+(defun twtxt--redraw-timeline ()
+  "Redraw the timeline."
+  (twtxt--clear-txts)
   ;; twtxts
   (dolist (twts (cl-subseq (twtxt--timeline)
 			   (* (- twtxt--twtxts-page 1) twtxt--twtxts-per-page)
@@ -112,28 +115,66 @@
       (when avatar-url (put-image-from-cache avatar-url (line-number-at-pos) 50))
       ;; nick + date
       (widget-insert (concat "  " nick " - " date " "))
-      (if thead (widget-create 'push-button
-			       :notify (lambda (&rest ignore) (message "Feature not yet implemented."))
-			       "Go to thread") (widget-create 'push-button " ↳ Reply "))
+      (if thead (widget-create-and-add 'push-button
+				       :notify (lambda (&rest ignore) (message "Feature not yet implemented."))
+				       "Go to thread") (widget-create-and-add 'push-button " ↳ Reply "))
       ;; Separator
       (widget-insert "\n")
       (widget-insert twtxt--timeline-separator)
       (widget-insert "\n")))
-
   ;; Navigation
   (widget-insert "\n")
-  (widget-create 'push-button
+  (widget-create-and-add 'push-button
 		 :notify (lambda (&rest ignore)
-			   (setq twtxt--twtxts-page (1+ twtxt--twtxts-page))
-			   (twtxt--timeline-layout))
+			     (twtxt--next-page))
 		 " Next page → ")
   (widget-insert "\n\n")
   (when (> twtxt--twtxts-page 1)
-    (widget-create 'push-button
+    (widget-create-and-add 'push-button
 		   :notify (lambda (&rest ignore)
-			     (setq twtxt--twtxts-page (1- twtxt--twtxts-page))
-			     (twtxt--timeline-layout))
+			     (twtxt--previous-page))
 		   " ← Previous page "))
+
+  )
+
+;; Macros
+(defmacro widget-create-and-add (&rest body)
+  "Create a widget and add it to the list of widgets."
+  `(add-to-list 'twtxt--widgets (widget-create ,@body)))
+
+;; Layout
+(defun twtxt--timeline-layout ()
+  "Create the main layout for the welcome screen."
+  (when (get-buffer twtxt--timeline-name-buffer)
+    (kill-buffer twtxt--timeline-name-buffer))
+  (switch-to-buffer twtxt--timeline-name-buffer)
+  (kill-all-local-variables)
+  ;; Delete old widgets
+  (let ((inhibit-read-only t))
+    (erase-buffer))
+  (remove-overlays)
+  ;; Controls
+  (twtxt-recalculate-timeline-separator)
+  (widget-insert "\n")
+  (widget-create 'push-button
+		 :notify (lambda (&rest ignore)
+			   (twtxt--post-buffer))
+		 :help-echo "Publish a new twtxt post."
+		 " ＋ New post ")
+  (widget-insert " ")
+  (widget-create 'push-button
+		 :notify (lambda (&rest ignore)
+			   (twtxt-timeline))
+		 " ↺ Refresh timeline ")
+  (widget-insert " ")
+  (widget-create 'push-button
+		 :notify (lambda (&rest ignore)
+			   (twtxt-timeline))
+		 " 🖼 Show profile ")
+  (widget-insert "\n\n")
+  (widget-insert twtxt--timeline-separator)
+  (widget-insert "\n\n")
+  (twtxt--redraw-timeline)
   (use-local-map widget-keymap)
   (widget-setup)
   (display-line-numbers-mode 0)
